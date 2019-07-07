c7aab87d3ec15750f1034852c7ac167008dbef44
heihuahe <1096910787@qq.com>
2019-07-08 02:26:16 +0800

    imageLoad

---
+++ demoList/imageLoading/lazyload.js
@@ -0,0 +1,53 @@
+/***
+ * 图片懒加载 手写
+ */
+function throttle(fn, delay, atleast) {//函数绑定在 scroll 事件上，当页面滚动时，避免函数被高频触发，
+  var timeout = null,//进行去抖处理
+  startTime = new Date();
+  return function() {
+  var curTime = new Date();
+  clearTimeout(timeout);
+  if(curTime - startTime >= atleast) {
+      fn();
+      startTime = curTime;
+  }else {
+      timeout = setTimeout(fn, delay);
+  }
+  }
+}
+function lazyload() {
+  var images = document.getElementsByTagName('img');
+  var len    = images.length;
+  var n      = 0;      //存储图片加载到的位置，避免每次都从第一张图片开始遍历        
+  return function() {
+  var seeHeight = document.documentElement.clientHeight;
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  for(var i = n; i < len; i++) {
+      if(images[i].offsetTop < seeHeight + scrollTop) {
+          if(images[i].getAttribute('src') === '../../img/loading.gif') {
+           images[i].src = images[i].getAttribute('data-src');
+          }
+      n = n + 1;
+       }
+  }
+  }
+}
+// lazyload()
+var loadImages = lazyload();
+loadImages();
+window.addEventListener('scroll', throttle(loadImages, 500, 1000), false);
+
+/**
+ * 插件 echo.js
+ * github地址：https://github.com/toddmotto/echo
+ * 不依赖jquery，一个简单的js懒加载插件，js体积小，快，使用简单，兼容至ie8+
+ * 在img标签里，添加data-echo属性，如果要用echo懒加载添加data-echo-background 属性就行。
+ */
+// echo.init({
+//   offset: 100,
+//   throttle: 250,
+//   unload: false,
+//   callback: function (element, op) {
+//     console.log(element, 'has been', op + 'ed')
+//   }
+// });
---
+++ demoList/imageLoading/preload.js
@@ -0,0 +1,7 @@
+/**
+ * 预加载3种方法
+ * 参考：https://www.csdn.net/article/2013-10-15/2817187-3-ways-preload-images-css-javascript-ajax
+ * 1.css 背景图
+ * 2.js new image，设置src加载
+ * 3.ajax预加载， new image对象设置src 
+ */
