63659602eea13623ccfbec47f476a3bb21974436
heihuahe <1096910787@qq.com>
2019-07-09 19:05:50 +0800

    imageLoad

--- demoList/imageLoading/index.js
+++ demoList/imageLoading/index.js
@@ -18,4 +18,22 @@ function currentPage(url) {
 window.onload = function() {
   var urlStr = location.href;
   this.currentPage(urlStr);
+  //css filter模糊处理
+  var placeholder = document.querySelector('.placeholder'),
+      small = placeholder.querySelector('.img-small')
+  
+  // 1: load small image and show it
+  var img = new Image();
+  img.src = small.src;
+  img.onload = function () {
+   small.classList.add('loaded');
+  };
+  
+  // 2: load large image
+  var imgLarge = new Image();
+  imgLarge.src = placeholder.dataset.large; 
+  imgLarge.onload = function () {
+    imgLarge.classList.add('loaded');
+  };
+  placeholder.appendChild(imgLarge);
 }
